14227df4f37b4ba41231eb86145465f7e9ff63c7
mac <toshi.import@gmail.com>
2018-05-28 23:44:51 +0900

    edit app/controllers/carts_controller.rb.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,36 +9,23 @@ class CartsController < ApplicationController
       @totalcount += cartitem.quantity
       @totalprice += (cartitem.quantity * cartitem.item.price)
     end
-
-
-
   end
 
   def add_item
       @cartitem =Cartitem.new(cartitem_params)
       @checkcartitem = Cartitem.find_by(item_id: @cartitem.item_id, user_id: current_user.id)
-      # binding.pry
 
       if @checkcartitem.blank?
         @cartitem.save
-        # binding.pry
         redirect_to carts_path, success: "#{@cartitem.item.itemName}がカートに追加されました"
       else
         @checkcartitem.update_columns(quantity: (@checkcartitem.quantity + @cartitem.quantity))
         @checkcartitem.save
-        # binding.pry
         redirect_to carts_path, success: "#{@checkcartitem.item.itemName}がカートに追加されました"
       end
   end
 
   private
-
-  # def set_cartitems
-  #   # @cartitem = Cartitem.new
-  #   @cartitem = current_user.cartitems.find_by(item_id: params[:item_id])
-  #   binding.pry
-  # end
-
   def cartitem_params
     params.permit(:quantity, :item_id, :user_id)
   end
